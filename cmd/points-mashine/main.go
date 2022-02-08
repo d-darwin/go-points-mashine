b1a80ac16d6316b85dfe5c8f94ed643b334d4360
Dmitriy Bykov <dmibykov@ozon.ru>
2022-02-08 07:51:46 +0300

    move account details to a separate package

--- cmd/points-mashine/main.go
+++ cmd/points-mashine/main.go
@@ -1,5 +1,42 @@
-package pointsmashine
+package main
+
+import (
+	"fmt"
+	"os"
+	points_mashine "pointsmashine/internal/app/points-mashine"
+)
 
 func main() {
+	// оператор defer выполняется после завершения работы функции
+	defer func() {
+		if v := recover(); v != nil {
+			// bad practice to intercept panics
+			fmt.Println("Panic happened", v)
+			os.Exit(1)
+		}
+	}()
 
+	// slice
+	accountsSlice := make([]int, 0, 1)
+	accountsSlice = append(accountsSlice, 100)
+	accountsSlice = append(accountsSlice, 200)
+	accountsSlice = append(accountsSlice, 300)
+	fmt.Println(accountsSlice)
+	fmt.Println(cap(accountsSlice))
+
+	// map
+	// var accountsMap map[int]int
+	// accountsMap[1] = 100
+	points_mashine.InitAccount(1)
+	points_mashine.Accrual(1, 100)
+	points_mashine.Charge(1, 50)
+	// accountsMap[2] = 200
+	points_mashine.InitAccount(2)
+	points_mashine.Accrual(2, 200)
+	points_mashine.Charge(2, 100)
+	// accountsMap[3] = 300
+	// initAccount(2)
+
+	fmt.Println(accountsMap)
+	points_mashine.PrintTransactions()
 }
---
+++ internal/app/points-mashine/points_mashine.go
@@ -0,0 +1,75 @@
+package points_mashine
+
+import (
+	"fmt"
+)
+
+var accountsMap = make(map[int]uint) // bad practice
+
+type transactionType uint8
+
+const (
+	AccrualType transactionType = iota + 1
+	ChargeType
+)
+
+type transaction struct {
+	clientId        int
+	transactionType transactionType
+	amount          uint
+}
+
+var transactions []*transaction
+
+func InitAccount(id int) {
+	amount, ok := accountsMap[id]
+	if ok {
+		panic("Account already exists") // bad practice to create panics
+	}
+	accountsMap[id] = amount
+}
+
+func PrintTransactions() {
+	fmt.Println("Transactions:")
+	for _, trx := range transactions {
+		fmt.Println(*trx)
+	}
+}
+
+func Accrual(id int, amount uint) {
+	if _, ok := accountsMap[id]; !ok {
+		panic("Account doesn't exists") // bad practice to create panics
+	}
+
+	accountsMap[id] += amount
+
+	trx := transaction{
+		clientId:        id,
+		transactionType: AccrualType,
+		amount:          amount,
+	}
+
+	transactions = append(transactions, &trx)
+}
+
+func Charge(id int, amount uint) {
+	currentAmount, ok := accountsMap[id]
+
+	if !ok {
+		panic("Account doesn't exists") // bad practice to create panics
+	}
+
+	if currentAmount < amount {
+		panic("Not enough points")
+	}
+
+	accountsMap[id] -= amount
+
+	trx := transaction{
+		clientId:        id,
+		transactionType: ChargeType,
+		amount:          amount,
+	}
+
+	transactions = append(transactions, &trx)
+}
